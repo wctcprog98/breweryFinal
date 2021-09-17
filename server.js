232dde8ab4227057da2c7cb42d7d4e63ffd33369
Nenye <chinenyediei@gmail.com>
2021-09-17 14:26:52 -0500

    testing new server connection

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express'); 
 const serveStatic = require('serve-static');
 const path = require('path');
-
+const post
 const app = express();
+
+// begin db connection
+const conn
+// end db connection
 
 app.use('/', serveStatic(path.join(__dirname, '/dist/')))
 
@@ -16,3 +20,22 @@ app.listen(port);
 
 console.log("Listening on port: " + port); 
 //testing for auto deploynent to heroku
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
