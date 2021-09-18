26404224ddf4b371690a50189d7ec34fc83981fc
Nenye <chinenyediei@gmail.com>
2021-09-17 21:54:52 -0500

    digital ocean 1st commit

--- server.js
+++ server.js
@@ -19,22 +19,3 @@ app.listen(port);
 
 console.log("Listening on port: " + port); 
 //testing for auto deploynent to heroku
-
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
